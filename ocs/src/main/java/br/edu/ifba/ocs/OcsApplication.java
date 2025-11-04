caa241f853529eb4b3cff2791c5c4962891b776b
RianBr07 <anthonyriandasilvaandradesilva@gmail.com>
2025-11-04 16:34:13 -0300

    Envio do projeto Spring Boot

---
+++ ocs/src/main/java/br/edu/ifba/ocs/OcsApplication.java
@@ -0,0 +1,15 @@
+package br.edu.ifba.ocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcsApplication.class, args);
+	}
+
+
+
+}
