538ae25389dd298f6ad21c2496717b1a2a78e893
RianBr07 <anthonyriandasilvaandradesilva@gmail.com>
2025-11-04 16:34:13 -0300

    Envio do projeto Spring Boot

---
+++ ocs/src/test/java/br/edu/ifba/ocs/OcsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.ocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
